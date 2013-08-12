22ad92a92bb3a1bbe24304d6bab166ed6acb1c40
Jeff Chang <jeffchang.ca@gmail.com>
2013-08-11 18:29:55 -0700

    fixed incorrect email and password bug

--- app/helpers/confirm_login.rb
+++ app/helpers/confirm_login.rb
@@ -1,11 +1,12 @@
 def confirm_login
+  session[:error] = []
   if User.where(email: params[:email]).count > 0
     if User.authenticate(params[:email], params[:password])
       session[:email] = params[:email]
     else
-      session[:error] = "Incorrect password.  Please try again!"
+      session[:error] << "Incorrect password.  Please try again!"
     end
   else
-    session[:error] = "Invalid email!  Please try again."
+    session[:error] << "Invalid email!  Please try again."
   end
 end
