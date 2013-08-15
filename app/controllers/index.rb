464f7b2ce21c15d97d0f6cd911332e1f927b721e
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-08-14 17:05:45 -0700

    update controller and views for creating a new post, adding a comment to a post

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,6 +13,17 @@ get '/:post_id' do
   erb :post
 end
 
+post '/:post_id' do
+  erb :post
+end
+
+get '/create_post' do
+  erb :create_post
+end
+
+post '/create_post' do
+  erb :index
+end
 
 #----------- SESSIONS -----------
 
